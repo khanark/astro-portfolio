bd21a3966932bbc01ff49ca288e03c79b60ff079
JScodeKiddie <JScodeKiddie@gmail.com>
2023-10-04 22:15:08 +0200

    create (layout): created layout for the posts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,10 +1,19 @@
 export const scrollToSection = (id: string) => {
-    const element = document.getElementById(id);
-    if (element) {
-        const rect = element.getBoundingClientRect();
-        window.scrollTo({
-            top: rect.top + window.scrollY - 20,
-            behavior: "smooth",
-        });
-    }
+  const element = document.getElementById(id);
+  if (element) {
+    const rect = element.getBoundingClientRect();
+    window.scrollTo({
+      top: rect.top + window.scrollY - 20,
+      behavior: "smooth",
+    });
+  }
+};
+
+export const formatDate = (date: string) => {
+  const d = new Date(date);
+  return d.toLocaleString("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
 };
