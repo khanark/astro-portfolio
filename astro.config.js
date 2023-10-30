f45cdd98c14ab37900711fec4575c826e62fa0e5
JScodeKiddie <JScodeKiddie@gmail.com>
2023-10-30 20:52:03 +0100

    remove netlify.toml file

--- astro.config.js
+++ astro.config.js
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
-  experimental: { assets: true, viewTransitions: true },
+    integrations: [react(), tailwind()],
+    experimental: { assets: true },
 });
